ac9286d822b59967ad47be4a75782810bc7efdcc
flo <flo.macicasan@gmail.com>
2022-05-04 19:44:13 +0300

    fruit becomes an imutable object

--- src/ro/fasttrack/lab11/hw/Fruit.java
+++ src/ro/fasttrack/lab11/hw/Fruit.java
@@ -5,13 +5,27 @@ package ro.fasttrack.lab11.hw;
  * @since 04.05.2022.
  */
 public class Fruit {
-    private String nume;
-    private int quantity;
+    private final String nume;
+    private final int quantity;
 
     public Fruit(String nume, int quantity) {
         this.nume = nume;
         this.quantity = quantity;
     }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // nu compileaza pentru ca quantity e final
+    // si nu poate sa se schimbe o data ce e initializat in constructor
+//    public void setQuantity(int newQuantity) {
+//        this.quantity = newQuantity;
+//    }
 
     @Override
     public String toString() {
