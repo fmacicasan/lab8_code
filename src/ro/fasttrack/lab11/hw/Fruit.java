f2d0dea37aa254ed530e8969157ee41ac858ca43
flo <flo.macicasan@gmail.com>
2022-05-04 21:23:45 +0300

    comparable implementation

--- src/ro/fasttrack/lab11/hw/Fruit.java
+++ src/ro/fasttrack/lab11/hw/Fruit.java
@@ -1,10 +1,27 @@
 package ro.fasttrack.lab11.hw;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author flo
  * @since 04.05.2022.
  */
-public class Fruit {
+public class Fruit implements Comparable<Fruit> {
+
+    public static void main(String[] args) {
+        List<Fruit> fruits = new ArrayList<>();
+        fruits.add(new Fruit("capsuni", 50));
+        fruits.add(new Fruit("banana", 5));
+        fruits.add(new Fruit("pere", 6));
+        fruits.add(new Fruit("mere", 2));
+        System.out.println(fruits);
+
+        Collections.sort(fruits);
+        System.out.println(fruits);
+
+    }
     private final String nume;
     private final int quantity;
 
@@ -33,5 +50,13 @@ public class Fruit {
                 "nume='" + nume + '\'' +
                 ", quantity=" + quantity +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Fruit o) {
+        //sort crescator
+        return this.nume.compareTo(o.nume);
+        //sort descrescator
+//        return -1 * this.nume.compareTo(o.nume);
     }
 }
